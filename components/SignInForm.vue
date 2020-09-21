ecfa33d74fc29cb737bed42331af019d27184cef
alohaloha <aloha_au_ia_oe_kai@yahoo.co.jp>
2020-09-21 20:56:55 +0900

    add signin page

---
+++ components/SignInForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <form class="form" @submit.prevent="$emit('submit', { email, password })">
+    <BaseInput v-model="email" type="email" placeholder="メールアドレス" />
+    <BaseInput v-model="password" type="password" placeholder="password" />
+    <BaseButton :disabled="isSending" class="mts">ログイン</BaseButton>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    isSending: Boolean,
+  },
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+}
+</script>
---
+++ pages/signin/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrap">
+    <SignInForm :is-sending="isSending" @submit="signIn" />
+    <BaseButton to="/" class="mts" is-txt>戻る</BaseButton>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isSending: false,
+    }
+  },
+  methods: {
+    signIn({ email, password }) {
+      // this.isSendingをtrueにする
+      console.log(email, password)
+      // email,passwordを使ってサーバーにリクエスト
+      // this.isSendingをfalseにする
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+  padding: 2.5rem 1rem 0;
+}
+</style>
